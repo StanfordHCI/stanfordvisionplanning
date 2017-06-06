c4c9a0092fb079b15fe1118991d2595b7f1960f5
niloufars <niloufar.salehi@gmail.com>
2017-06-06 14:53:57 -0700

    deleting votes

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,6 +1,6 @@
 class Idea < ApplicationRecord
   belongs_to :user
-  has_many :votes, dependent: :destroy
+  has_many :votes, dependent: :delete_all
   belongs_to :thredded_topic,  class_name: 'Thredded::Topic'
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
