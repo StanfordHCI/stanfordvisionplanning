635baf15f88112122c874d81ead600e93127cefd
niloufars <niloufar.salehi@gmail.com>
2017-06-06 12:31:28 -0700

    fixed users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.order(:id).paginate(page: params[:page], per_page: 30)
   end
 
   # GET /users/1
