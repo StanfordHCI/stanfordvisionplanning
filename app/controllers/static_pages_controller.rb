5f1cf276b09d7bc4f069932d6ed7954baae5ea9b
niloufars <niloufar.salehi@gmail.com>
2017-06-20 13:17:28 -0700

    don't paginate

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
   def home
     @idea = current_user.ideas.build     if logged_in?
     @recent_ideas = Idea.order( 'created_at DESC' ).take(3)
-    @ideas = (Idea.all.sort {|a,b| a.votes_value <=> b.votes_value}.reverse-@recent_ideas).paginate(page: params[:page])
+    @ideas = (Idea.all.sort {|a,b| a.votes_value <=> b.votes_value}.reverse-@recent_ideas)
     @ideas = @ideas.insert(2, *@recent_ideas)
   end
   def faq
