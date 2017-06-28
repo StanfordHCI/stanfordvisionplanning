51f052954885da73d0fe5bf892c4ec07aa654e8e
niloufars <niloufar.salehi@gmail.com>
2017-06-28 16:29:07 -0700

    testing new idea ordering

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,15 @@ class StaticPagesController < ApplicationController
   require 'will_paginate/array'
   def home
     @idea = current_user.ideas.build     if logged_in?
-    @recent_ideas = Idea.order( 'created_at DESC' ).take(3)
-    @ideas = (Idea.all.sort {|a,b| a.votes_value <=> b.votes_value}.reverse-@recent_ideas)
-    @ideas = @ideas.insert(2, *@recent_ideas)
+    @recent_ideas = Idea.order( 'created_at DESC' ).take(30)
+    @ideas = (Idea.all.sort {|a,b| a.votes_value <=> b.votes_value}.reverse)
+    @recent_ideas.each do |ri| 
+    	if rand(3) == 0
+    		@ideas = @ideas-[ri]
+			@ideas = @ideas.insert(rand(@ideas.length/2), ri)    		
+    	end
+    end
+
   end
   def faq
   end
